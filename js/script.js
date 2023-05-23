385eb40683e10561ba471aea90ec97120b522e11
GustavoHMoraes <gustavo.rocillo@etec.sp.gov.br>
2023-05-23 12:15:07 -0300

    atualizacao

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById('canva');
 const ctx = canvas.getContext('2d');
+
 ctx.lineWidth = 5;
 ctx.moveTo(550,150);
 ctx.lineTo(350,225);
@@ -22,15 +23,24 @@ ctx.lineTo(210,300);
 ctx.stroke();
 
 ctx.beginPath();
+ctx.fillStyle="green"
 ctx.lineWidth = 5;
 ctx.moveTo(100, 430);
 ctx.quadraticCurveTo(50, 410, 200, 380);
 ctx.stroke();
+ctx.fill();
 
 ctx.beginPath();
+ctx.fillStyle="red"
 ctx.lineWidth = 5;
 ctx.moveTo(100, 430);
 ctx.quadraticCurveTo(300, 440, 250, 390);
+ctx.stroke();
+ctx.fill()
+
+ctx.beginPath();
+ctx.arc(100, 210, 70, 0, 2 * Math.PI);
+ctx.lineWidth = 10;
 ctx.stroke();
 
 ctx.lineWidth = 5;
@@ -38,7 +48,39 @@ ctx.moveTo(200,380);
 ctx.lineTo(250,390);
 ctx.stroke();
 
+ctx.lineWidth = 5;
+ctx.strokeStyle ="black";
+ctx.fillStyle ="black";
 ctx.beginPath();
-ctx.arc(100, 210, 70, 0, 2 * Math.PI);
-ctx.lineWidth = 10;
+ctx.moveTo(100, 160);
+ctx.lineTo(145, 198);
+ctx.lineTo(130, 240);
+ctx.lineTo(70, 240);
+ctx.lineTo(55, 198);
+ctx.closePath();
+ctx.stroke();
+
+ctx.lineWidth = 5;
+ctx.moveTo(100,160);
+ctx.lineTo(100,145);
+ctx.stroke();
+
+ctx.lineWidth = 5;
+ctx.moveTo(145,198);
+ctx.lineTo(170,198);
+ctx.stroke();
+
+ctx.lineWidth = 5;
+ctx.moveTo(130,240);
+ctx.lineTo(155,250);
+ctx.stroke();
+
+ctx.lineWidth = 5;
+ctx.moveTo(70,240);
+ctx.lineTo(45,250);
+ctx.stroke();
+
+ctx.lineWidth = 5;
+ctx.moveTo(55,198);
+ctx.lineTo(30,198);
 ctx.stroke();
